426c0e5873f1067bd7a2df6526a29c0792951fc1
PIjus-Github <92991997+PIjus-Github@users.noreply.github.com>
2021-11-11 16:33:11 +0200

    file

---
+++ Wayback-Machine-Popup-Remover.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Wayback Machine Popup Remover
+// @namespace    https://github.com/PIjus-Github
+// @version      1.0
+// @description  Removes any popups that you may see on Wayback Machine
+// @author       PIjus
+// @match        https://*.web.archive.org/*
+// @grant        https://*.web.archive.org/*
+// ==/UserScript==
+var elem = document.getElementById("donate_banner");
+  elem.parentNode.removeChild(elem);
